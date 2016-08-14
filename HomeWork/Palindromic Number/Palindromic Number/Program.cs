de2febf6bc9ad3956acefd0f733a815307286bb7
yuliyanechet <yuliyanechet@yandex.ua>
2016-08-14 21:20:31 +0300

    finish

---
+++ HomeWork/Palindromic Number/Palindromic Number/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromic_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentNumber;
+            List<int> found = new List<int>();
+
+            for (int i = 100; i <= 999; i++)
+            {
+                for (int j = 100; j <= 999; j++)
+                {
+                    currentNumber = i * j;
+                    string temp = currentNumber.ToString();
+                    if (temp.Length == 6)
+                    {
+                        if (temp.Substring(0, 1) == temp.Substring(5,1) && temp.Substring(1, 1) == temp.Substring(4, 1) && temp.Substring(2, 1) == temp.Substring(3, 1)) 
+                        {
+                            found.Add(Convert.ToInt32(temp));
+                        }
+                    }
+
+                  }
+            }
+            int theBiggest = 0;
+            foreach (var item in found)
+            {
+                if (theBiggest < item)
+                {
+                    theBiggest = item;
+                }
+            }
+            Console.WriteLine(theBiggest);
+        }
+    }
+}
+
---
+++ HomeWork/smallestMultiple/smallestMultiple/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smallestMultiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            for (int number = 1; ; number++)
+            {
+                bool state = true;
+                foreach (var item in arr)
+                {
+                    if (number % item != 0)
+                    {
+                        state = false;
+                        break;
+                    }
+
+                }
+
+                if(state)
+                {
+                    Console.WriteLine(number);
+                    break;
+                }
+            }
+
+        }
+    }
+}
+   
