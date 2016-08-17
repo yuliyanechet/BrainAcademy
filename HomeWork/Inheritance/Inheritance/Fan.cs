3d6ba450a7ce86b21666f56acf1270ece4978f5d
Oksana <kol.oksana81@gmail.com>
2016-08-17 21:18:25 +0300

    5

---
+++ HomeWork/Inheritance/Inheritance/Fan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Fan: Human
+    {
+        public Fan(DateTime birthdate, Address birhtplace, params Band[] band)
+            :base (birthdate, birhtplace)
+        {
+
+        }
+
+        public Band[] Groups { get; set; }
+
+
+        public void Shout()
+        {
+            string str = "";
+            Console.WriteLine("I can shout");
+               
+        }
+    }
+}
