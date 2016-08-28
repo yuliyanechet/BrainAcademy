4d587d3677d0cd52addfc08082e3eb7c44ee1802
yuliyanechet <yuliyanechet@yandex.ua>
2016-08-28 20:20:30 +0300

    ArrayList Method

---
+++ HomeWork/ConsoleApplication2/ConsoleApplication2/MyClass.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class MyClass
+    {
+        public object[] arr;
+        int count = 0;
+
+        public void Delete(int index)
+        {
+            object[] temp = new object[arr.Length - 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (index != i)
+                {
+                    temp[count] = arr[i];
+                    count++;
+                };
+            }
+            count = 0;
+            arr = temp;
+        }
+
+        public void AddRange(object[] addElements)
+        {
+            object[] temp = new object[arr.Length + addElements.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                temp[i] = arr[i];
+            }
+            for (int j = 0; j < addElements.Length; j++)
+            {
+                temp[j + arr.Length] = addElements[j];
+            }
+
+            arr = temp;
+        }
+
+        public bool Contain(object search)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].ToString() == search.ToString())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void IndexOf(object number)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].ToString() == number.ToString())
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public void Sort()
+        {
+            int[] temp = new int[arr.Length];
+
+            for (int x=0; x< arr.Length;x++)
+            {
+                temp[x] = Convert.ToInt32(arr[x]);
+            }
+            for (int j = 0; j < arr.Length-1; j++)
+            {
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    if (temp[i - 1] > temp[i])
+                    {
+                        int third = temp[i - 1];
+                        temp[i - 1] = temp[i];
+                        temp[i] = third;
+                    }
+                }            
+            }
+            for (int i = 0; i < temp.Length; i++)
+            {
+                Console.WriteLine(temp[i]);
+            }
+        }
+    }
+}
---
+++ HomeWork/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] arr = new object[8] { 5, 40, 31, 5, 2 , 23, 45, 1};
+            MyClass myclass = new MyClass();
+            myclass.arr = arr;
+            //Console.WriteLine(myclass.arr.Length);
+            //myclass.Delete(3);
+            //myclass.Delete(1);
+            //Console.WriteLine(myclass.arr.Length);
+
+            //myclass.AddRange(new object[4] { 1, 2, 3, 4 });
+            //foreach (var item in myclass.arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine(myclass.Contain(1));
+
+            //myclass.IndexOf(3);
+            myclass.Sort();
+        }
+    }
+}
+  
